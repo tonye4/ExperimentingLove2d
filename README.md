1c3eaf06071d730b7ea3c0142005acc7010de2b1
tonye4 <138623110+tonye4@users.noreply.github.com>
2024-12-30 14:57:56 -0500

    Change to topdown view and add sprinting

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ExperimentingLove2d
 Just messing around with love2d and learning Lua
+
+## Dec 30, 2024 
+I wanna make the game have a topdown view kinda like Stardew. I figured out the movement and now wanna add inventory and interacting with items. 
+- [ ] TODO: Fix code and implement better Lua/LOVE practices. 
+- [ ] TODO: Fix the infinite running bug.
+- [ ] TODO: Figure out how the inventory system should work.
--- main.lua
+++ main.lua
@@ -1,62 +1,46 @@
-platform = {}
+item = {}
 player = {}
+inventory = {}
 
 function love.load()
-	platform.width = love.graphics.getWidth()
-	platform.height = love.graphics.getHeight() -- makes platform height as big as window. 
-
-	platform.x = 0 
-	platform.y = platform.height/2 -- this is to draw the platform. 
-
-	player.x = love.graphics.getWidth() / 2 
-	player.y = love.graphics.getHeight() / 2 -- positions player in the middle fo the screen 
-
-	player.img = love.graphics.newImage('purple.png')
-
-	player.speed = 300 
-
-	-- jumping mechanics 
-	player.ground = player.y 
-	player.y_velocity = 0 -- speed in a direction 
-
-	player.jump_height = -400 -- for some reason negative 
-	player.gravity = -2000 -- character descends at this rate. Gravity should generally be greater than jump_height. 
-end 
+    player.x = love.graphics.getWidth() / 2
+    player.y = love.graphics.getHeight() / 2
+    player.img = love.graphics.newImage('purple.png')
+    player.speed = 200
+    player.stamina = 200
+    -- player.recovery = true
+end
 
 function love.update(dt)
-	-- Movement along the x-axis
-	if love.keyboard.isDown('d') then
-		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then  -- 
-			player.x = player.x + (player.speed * dt) -- multiplying by delta time, makes sure that the movement across frame rates is consistent. 
-		end 
-	elseif love.keyboard.isDown('a') then
-		if player.x > 0 then -- pixels in love2d are rooted at 0, so the xth position at 0, would be the left most bound.
-			player.x = player.x - (player.speed * dt)
-		end 
-	end 
-	-- jumping logic 
-	if love.keyboard.isDown('space') then 
-		if player.y_velocity == 0 then -- players velocity is == 0 when they're on the ground.  
-			player.y_velocity = player.jump_height -- if they're on the ground, their position can change to the jump_height. 
-		end
-	end 
+    -- top down movement.
+    if love.keyboard.isDown('d') then
+        player.x = player.x + (player.speed * dt)
+    end
 
-	if player.y ~= 0 then -- If player has already left the ground.  
-		player.y = player.y + player.y_velocity * dt -- this is the actual jumping. 
-		player.y_velocity = player.y_velocity - player.gravity * dt -- gravity applied to player
-	end 
+    if love.keyboard.isDown('a') then
+        player.x = player.x - (player.speed * dt)
+    end
 
-	if player.y > player.ground then -- While the player is in the air  
-		player.y_velocity = 0 -- if they jumped the velocity is set to 0, which means they're back on the ground. 
-		player.y = player.ground 
-	end
+    if love.keyboard.isDown('w') then
+        player.y = player.y - (player.speed * dt)
+    end
 
-end 
+    if love.keyboard.isDown('s') then
+        player.y = player.y + (player.speed * dt)
+    end
+
+    if love.keyboard.isDown('lshift') and player.stamina > 0 then -- fix infinite sprint bug
+        player.speed = 400
+        player.stamina = player.stamina - 1
+    else
+        player.speed = 200
+        if player.stamina < 200 then
+            player.stamina = player.stamina + 1
+        end
+        end
+    end
 
 function love.draw()
-	love.graphics.setColor(1,1,1)
-
-	love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
-
-	love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32) -- using draw because we're loading an image. 
-end 	
+    love.graphics.draw(player.img, player.x, player.y, 0,1,1,0,32)
+    love.graphics.print(player.stamina, 0,0)
+end
