c3d338956170e0431e79bbb320dfd4077c217843
tonye4 <138623110+tonye4@users.noreply.github.com>
2024-12-30 17:25:28 -0500

    Make Person table a class

--- main.lua
+++ main.lua
@@ -1,47 +1,16 @@
-local player = {}
-local inventory = {} -- Use 'classic' instead. 
+--! File: Main
 
 function love.load()
     Object = require"classic" -- classic returns an Object
 
-    player.x = love.graphics.getWidth() / 2
-    player.y = love.graphics.getHeight() / 2
-    player.img = love.graphics.newImage('purple.png')
-    player.speed = 200
-    player.stamina = 200
-    -- player.recovery = true
+    require"player"
+    Player1 = Player()
 end
 
 function love.update(dt)
-    -- top down movement.
-    if love.keyboard.isDown('d') then
-        player.x = player.x + (player.speed * dt)
-    end
-
-    if love.keyboard.isDown('a') then
-        player.x = player.x - (player.speed * dt)
-    end
-
-    if love.keyboard.isDown('w') then
-        player.y = player.y - (player.speed * dt)
-    end
-
-    if love.keyboard.isDown('s') then
-        player.y = player.y + (player.speed * dt)
-    end
-
-    if love.keyboard.isDown('lshift') and player.stamina > 0 then -- fix infinite sprint bug
-        player.speed = 400
-        player.stamina = player.stamina - 1
-    else
-        player.speed = 200
-        if player.stamina < 200 then
-            player.stamina = player.stamina + 1
-        end
-        end
+    Player1:update(dt)
     end
 
 function love.draw()
-    love.graphics.draw(player.img, player.x, player.y, 0,1,1,0,32)
-    love.graphics.print(player.stamina, 0,0)
+    Player1:draw()
 end
--- player.lua
+++ player.lua
@@ -1,6 +1,46 @@
---! file: player
+--! file: Player
 
 Player = Object:extend()
 
 function Player:new()
-    self.x = 
+    self.x = love.graphics.getWidth() / 2
+    self.y = love.graphics.getHeight() / 2
+    self.img = love.graphics.newImage('purple.png')
+    self.speed = 200
+    self.stamina = 200
+end
+
+function Player:update(dt)
+    -- top down movement.
+    if love.keyboard.isDown('d') then
+        self.x = self.x + (self.speed * dt)
+    end
+
+    if love.keyboard.isDown('a') then
+        self.x = self.x - (self.speed * dt)
+    end
+
+    if love.keyboard.isDown('w') then
+        self.y = self.y - (self.speed * dt)
+    end
+
+    if love.keyboard.isDown('s') then
+        self.y = self.y + (self.speed * dt)
+    end
+
+    if love.keyboard.isDown('lshift') and self.stamina > 0 then -- fix infinite sprint bug
+        self.speed = 400
+        self.stamina = self.stamina - 1
+    else
+        self.speed = 200
+        if self.stamina < 200 then
+            self.stamina = self.stamina + 1
+        end
+    end
+end
+
+function Player:draw()
+    love.graphics.draw(self.img, self.x, self.y, 0,1,1,0,32)
+    love.graphics.print(self.stamina, 0,0)
+end
+
